c70e5c35dd0d77edc7e3579be9bd664ffcf278d2
Jeff <Jafaragha77@gmail.com>
2023-02-10 13:38:32 +0330

    SocerTile.kt and Recycler added

---
+++ app/src/main/java/com/example/epl/SocerTile.kt
@@ -0,0 +1,10 @@
+package com.example.epl
+
+data class SocerTile(
+    val id: String = "",
+    val headerImageResourceId: Int = 0,
+    val title: String = "",
+    val description: String = "",
+    val descriptionLong: String = "",
+    val buttonText: String = ""
+)
